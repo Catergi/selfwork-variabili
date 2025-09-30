6a5c7cadbd00880d3a075ca7bc5afb0279828f9a
Catergi <caterinadeam@gmail.com>
2025-09-30 17:28:19 +0200

    Correzione: Aggiunti file indexcorretto.html e main.js con selfwork variabili

---
+++ main.js
@@ -0,0 +1,23 @@
+ // Dichiarazione delle varibili numeriche
+        let num1 = 10;
+        let num2 = 15;
+        
+        // Visualizza a schermo i valori
+        console.log(num1);
+        console.log(num2);
+
+        // Variabile stringa
+        let stringa = 'Ciao, sono la prima stringa!'
+        console.log(stringa);
+
+        // Riassegno valore alla variabile stringa
+        stringa = 'Ciao, ho cambiato il valore della stringa!'
+        console.log(stringa);
+
+        // Costante PIGRECO
+        const PIGRECO = 3.14159265359;
+        console.log(PIGRECO);
+
+        // Riassegno valore della costante PIGRECO, Uncaught TypeError: Assignment to constant variable.    
+        PIGRECO = 3.15;
+        // console.log(PIGRECO); 
